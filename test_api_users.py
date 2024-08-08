00c75888a87130cec7167c057bfd779cbf5df99a
easyflygit <akv888@inbox.ru>
2024-08-08 15:10:49 +0200

    добавил второй тест на получение пользователя по id

--- test_api_users.py
+++ test_api_users.py
@@ -16,3 +16,24 @@ def test_get_users():
         assert "first_name" in data["data"][0]
         assert "last_name" in data["data"][0]
         assert "avatar" in data["data"][0]
+
+
+def test_get_user_by_id():
+    """Тест на получение пользователя с заданным id, например 'id=2'"""
+    user_id = 2
+    response = requests.get(f"{BASE_URL}/users/{user_id}")
+    assert response.status_code == 200
+
+    data = response.json()
+    assert "data" in data
+    user_data = data["data"]
+
+    # Проверяем, что id в данных ответа соответствует ожидаемому
+    assert "id" in user_data
+    assert user_data["id"] == user_id
+
+    # Проверяем наличие остальных полей
+    assert "email" in user_data
+    assert "first_name" in user_data
+    assert "last_name" in user_data
+    assert "avatar" in user_data
